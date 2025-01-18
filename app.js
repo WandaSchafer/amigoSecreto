7ce0be354a0161b2d87933d6124674ad2f0e341c
WandaSchafer <141286298+WandaSchafer@users.noreply.github.com>
2025-01-17 22:33:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-// Selección de elementos DOM
+
 const inputAmigo = document.getElementById('amigo');
 const listaAmigos = document.getElementById('listaAmigos');
 const resultado = document.getElementById('resultado');
